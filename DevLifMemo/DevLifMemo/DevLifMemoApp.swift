ac8d8a8fbb54469bea4dbabfe6b331f0b40a3cfa
SooHoon <dkehskeh@gmail.com>
2025-05-11 11:00:39 +0900

    create Memo Proj

---
+++ DevLifMemo/DevLifMemo/DevLifMemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  DevLifMemoApp.swift
+//  DevLifMemo
+//
+//  Created by 최수훈 on 5/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DevLifMemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
